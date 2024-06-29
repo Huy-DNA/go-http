50bd6d84d623819643b2aa0e8097d581af77e6fc
Huy-DNA <huydo862003@gmail.com>
2024-06-29 18:42:54 +0700

    refactor: move StartServer to http-server.go

--- http-server.go
+++ http-server.go
@@ -7,10 +7,19 @@ import (
 	"syscall"
 )
 
+
 type HttpServer struct {
   HttpConfiguration
   sockFd uint16
   epollFd uint16
+}
+
+func StartServer(config HttpConfiguration) HttpServer {
+  return HttpServer{
+    HttpConfiguration: config,
+    sockFd: 0,
+    epollFd: 0,
+  }
 }
 
 func (server *HttpServer) Listen() (err error) {
--- index.go
+++
@@ -1,9 +0,0 @@
-package http_server
-
-func StartServer(config HttpConfiguration) HttpServer {
-  return HttpServer{
-    HttpConfiguration: config,
-    sockFd: 0,
-    epollFd: 0,
-  }
-}
