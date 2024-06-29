295ab915773b97171198bbc8ee2c56a9e02de0cd
Huy-DNA <huydo862003@gmail.com>
2024-06-29 18:23:08 +0700

    test: augment TestHttpServerListen

--- http-server_test.go
+++ http-server_test.go
@@ -1,8 +1,10 @@
 package http_server_test
 
 import (
+	"fmt"
 	"net"
 	"testing"
+	"time"
 
 	go_http "github.com/Huy-DNA/go-http"
 	"github.com/stretchr/testify/assert"
@@ -30,7 +32,7 @@ func TestBuildConfiguration(t *testing.T) {
   }, "Built config should be equal to explicitly init config")
 }
 
-func TestHttpServerListen(t *testing.T) {
+func TestHttpServerConn(t *testing.T) {
   assert := assert.New(t)
 
   config := (go_http.HttpConfiguration{Verbose: true}).Build()
@@ -39,4 +41,16 @@ func TestHttpServerListen(t *testing.T) {
   error := server.Listen()
 
   assert.Nil(error, "Error should be nil");
+
+  go server.Accept()
+
+  time.Sleep(1000)
+  simulateTcpConnect(8000)
+  time.Sleep(1000)
+}
+
+func simulateTcpConnect(port uint16) {
+  servAddr := fmt.Sprintf("localhost:%v", port)
+  tcpAddr, _ := net.ResolveTCPAddr("tcp", servAddr)
+  net.DialTCP("tcp", nil, tcpAddr)
 }
