b1734753045d0c275197115aabc2851478042bcb
Huy-DNA <huydo862003@gmail.com>
2024-06-29 12:26:44 +0700

    feat: basic listen method for HttpServer

--- http-configuration.go
+++ http-configuration.go
@@ -2,4 +2,5 @@ package http_server
 
 type HttpConfiguration struct {
   Port int16
+  Backlog uint64      `default:"100000"`
 }
--- http-server.go
+++ http-server.go
@@ -1,5 +1,23 @@
 package http_server
 
+import (
+  "syscall"
+)
+
 type HttpServer struct {
   HttpConfiguration
 }
+
+func (server *HttpServer) Listen() (err error) {
+  config := server.HttpConfiguration
+  sockFd, error := syscall.Socket(syscall.AF_INET6, syscall.SOCK_STREAM, 0)
+  
+  if error != nil {
+    return error
+  }
+  
+  syscall.Listen(sockFd, int(config.Backlog))
+
+  return nil
+}
+
