c65621f591a27f6e043303a6e03cd0bfb53ec5b5
Huy-DNA <huydo862003@gmail.com>
2024-06-29 13:43:45 +0700

    test: add test build configuration

---
+++ http-server_test.go
@@ -0,0 +1,31 @@
+package http_server_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+  go_http "github.com/Huy-DNA/go-http"
+)
+
+func TestBuildConfiguration(t *testing.T) {
+  assert := assert.New(t)
+
+  config := (go_http.HttpConfiguration{
+    Port: 80,
+  }).Build();
+
+  assert.EqualExportedValues(config, go_http.HttpConfiguration{
+    Port: 80,
+    Backlog: 100000,
+  }, "Built config should be equal to explicitly init config");
+
+  config = (go_http.HttpConfiguration{}).Build();
+
+  assert.EqualExportedValues(config, go_http.HttpConfiguration{
+    Port: 8000,
+    Backlog: 100000,
+  }, "Built config should be equal to explicitly init config");
+}
+
+func TestHttpServerListent(t *testing.T) {
+    
+}
