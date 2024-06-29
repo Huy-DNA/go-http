a8a1afc3b337827f8089a7c557da35061a908ad2
Huy-DNA <huydo862003@gmail.com>
2024-06-29 13:41:33 +0700

    fix: Build method of http-configuration

--- http-configuration.go
+++ http-configuration.go
@@ -6,16 +6,21 @@ import (
 )
 
 type HttpConfiguration struct {
-  Port int16
+  Port uint16         `default:"8000"`
   Backlog uint64      `default:"100000"`
 }
 
-func (config *HttpConfiguration) Build() HttpConfiguration {
+func (config HttpConfiguration) Build() HttpConfiguration {
   typ := reflect.TypeOf(config)
 
   port := config.Port
-  backlog := config.Backlog
+  if port == 0 {
+    f, _ := typ.FieldByName("Port")
+    _port, _ := strconv.ParseUint(f.Tag.Get("default"), 10, 16)
+    port = uint16(_port)
+  }
 
+  backlog := config.Backlog
   if backlog == 0 {
     f, _ := typ.FieldByName("Backlog")
     backlog, _ = strconv.ParseUint(f.Tag.Get("default"), 10, 64)
