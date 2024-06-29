37bbee3d5a20b19214b589171d3db19f1b51e5e2
Huy-DNA <huydo862003@gmail.com>
2024-06-29 13:23:58 +0700

    feat: add Build method of HttpConfiguration

--- http-configuration.go
+++ http-configuration.go
@@ -1,6 +1,28 @@
 package http_server
 
+import (
+	"reflect"
+	"strconv"
+)
+
 type HttpConfiguration struct {
   Port int16
   Backlog uint64      `default:"100000"`
+}
+
+func (config *HttpConfiguration) Build() HttpConfiguration {
+  typ := reflect.TypeOf(config)
+
+  port := config.Port
+  backlog := config.Backlog
+
+  if backlog == 0 {
+    f, _ := typ.FieldByName("Backlog")
+    backlog, _ = strconv.ParseUint(f.Tag.Get("default"), 10, 64)
+  }
+
+  return HttpConfiguration{
+    Port: port,
+    Backlog: backlog,
+  }
 }
