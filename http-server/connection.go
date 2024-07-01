074d801ba3f16cb7b76c5b8b73f82456a02945b3
Huy-DNA <huydo862003@gmail.com>
2024-07-01 18:34:56 +0700

    feat: implement Start and Stop

---
+++ http-server/connection.go
@@ -0,0 +1,9 @@
+package http_server
+
+import (
+	"github.com/Huy-DNA/go-http/server-core"
+)
+
+type HttpConnection struct {
+  conn <-chan *server.Connection 
+}
--- http-server/http-server.go
+++ http-server/http-server.go
@@ -17,3 +17,15 @@ func New(config HttpServerConfiguration) HttpServer {
     server: server.New(config.Configuration),
   }
 }
+
+func (server *HttpServer) Start() (connChan *HttpConnection, err error) {
+  conn, error := server.server.Start()
+  if error != nil {
+    return nil, error
+  }
+  return &HttpConnection{conn: conn}, nil
+}
+
+func (server *HttpServer) Stop() {
+  server.server.Stop()
+}
